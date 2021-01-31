417913fe588f0167c4ff1aa0ae2ff238a1519e6e
Christian <christianvillere@gmail.com>
2021-01-30 22:41:24 -0800

    Add function to create divs

--- EAS.js
+++ EAS.js
@@ -1,2 +1,12 @@
 const containerDiv = document.body.querySelector(".container");
+const numberToSquare = 16;
+const squaredNumber = 256;
+
+function generateChildDivs () {
+  for (i = 0; i < squaredNumber; i++) {
+    const childDiv = document.createElement("div");
+    containerDiv.appendChild(childDiv);
+  }
+
+}
 
