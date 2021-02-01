d500ab7bf8bfcdadeefc92bebb2731ca91fdea74
Christian <christianvillere@gmail.com>
2021-01-31 20:54:03 -0800

    Create 16x16 grid of square divs

--- EAS.js
+++ EAS.js
@@ -5,8 +5,14 @@ const squaredNumber = 256;
 function generateChildDivs () {
   for (i = 0; i < squaredNumber; i++) {
     const childDiv = document.createElement("div");
+    childDiv.classList.add("cDiv")
     containerDiv.appendChild(childDiv);
   }
 
 }
+
+generateChildDivs();
+
+
+
 
